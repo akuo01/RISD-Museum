edbed4238b5e5a95f70d867dd0cafb37bf2630c0
akuo01 <akuo01@risd.edu>
2020-01-27 22:58:54 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,512 @@
+function song1enter() {
+    var song1 = document.querySelector("#song1");
+    song1.play();
+}
+function song1leave () {
+    var song1 = document.querySelector("#song1");
+    song1.pause();
+}
+function song2enter() {
+    var song2 = document.querySelector("#song2");
+    song2.play();
+}
+function song2leave () {
+    var song2 = document.querySelector("#song2");
+    song2.pause();
+}
+function song3enter() {
+    var song3 = document.querySelector("#song3");
+    song3.play();
+}
+function song3leave () {
+    var song3 = document.querySelector("#song3");
+    song3.pause();
+}
+function song4enter() {
+    var song4 = document.querySelector("#song4");
+    song4.play();
+}
+function song4leave () {
+    var song4 = document.querySelector("#song4");
+    song4.pause();
+}
+function song5enter() {
+    var song5 = document.querySelector("#song5");
+    song5.play();
+}
+function song5leave () {
+    var song5 = document.querySelector("#song5");
+    song5.pause();
+}
+function song6enter() {
+    var song6 = document.querySelector("#song6");
+    song6.play();
+}
+function song6leave () {
+    var song6 = document.querySelector("#song6");
+    song6.pause();
+}
+function song7enter() {
+    var song7 = document.querySelector("#song7");
+    song7.play();
+}
+function song7leave () {
+    var song7 = document.querySelector("#song7");
+    song7.pause();
+}
+function song8enter() {
+    var song8 = document.querySelector("#song8");
+    song8.play();
+}
+function song8leave () {
+    var song8 = document.querySelector("#song8");
+    song8.pause();
+}
+function song9enter() {
+    var song9 = document.querySelector("#song9");
+    song9.play();
+}
+function song9leave () {
+    var song9 = document.querySelector("#song9");
+    song9.pause();
+}
+function song10enter() {
+    var song10 = document.querySelector("#song10");
+    song10.play();
+}
+function song10leave () {
+    var song10 = document.querySelector("#song10");
+    song10.pause();
+}
+function song11enter() {
+    var song11 = document.querySelector("#song11");
+    song11.play();
+}
+function song11leave () {
+    var song11 = document.querySelector("#song11");
+    song11.pause();
+}
+function song12enter() {
+    var song12 = document.querySelector("#song12");
+    song12.play();
+}
+function song12leave () {
+    var song12 = document.querySelector("#song12");
+    song12.pause();
+}
+function song13enter() {
+    var song13 = document.querySelector("#song13");
+    song13.play();
+}
+function song13leave () {
+    var song13 = document.querySelector("#song13");
+    song13.pause();
+}
+function song14enter() {
+    var song14 = document.querySelector("#song14");
+    song14.play();
+}
+function song14leave () {
+    var song14 = document.querySelector("#song14");
+    song14.pause();
+}
+function song15enter() {
+    var song15 = document.querySelector("#song15");
+    song15.play();
+}
+function song15leave () {
+    var song15 = document.querySelector("#song15");
+    song15.pause();
+}
+function song16enter() {
+    var song16 = document.querySelector("#song16");
+    song16.play();
+}
+function song16leave () {
+    var song16 = document.querySelector("#song16");
+    song16.pause();
+}
+function song17enter() {
+    var song17 = document.querySelector("#song17");
+    song17.play();
+}
+function song17leave () {
+    var song17 = document.querySelector("#song17");
+    song17.pause();
+}
+function song18enter() {
+    var song18 = document.querySelector("#song18");
+    song18.play();
+}
+function song18leave () {
+    var song18 = document.querySelector("#song18");
+    song18.pause();
+}
+function song19enter() {
+    var song19 = document.querySelector("#song19");
+    song19.play();
+}
+function song19leave () {
+    var song19 = document.querySelector("#song19");
+    song19.pause();
+}
+function song20enter() {
+    var song20 = document.querySelector("#song20");
+    song20.play();
+}
+function song20leave () {
+    var song20 = document.querySelector("#song20");
+    song20.pause();
+}
+function song21enter() {
+    var song21 = document.querySelector("#song21");
+    song21.play();
+}
+function song21leave () {
+    var song21 = document.querySelector("#song21");
+    song21.pause();
+}
+function song22enter() {
+    var song22 = document.querySelector("#song22");
+    song22.play();
+}
+function song22leave () {
+    var song22 = document.querySelector("#song22");
+    song22.pause();
+}
+function song23enter() {
+    var song23 = document.querySelector("#song23");
+    song23.play();
+}
+function song23leave () {
+    var song23 = document.querySelector("#song23");
+    song23.pause();
+}
+function song24enter() {
+    var song24 = document.querySelector("#song24");
+    song24.play();
+}
+function song24leave () {
+    var song24 = document.querySelector("#song24");
+    song24.pause();
+}
+function song25enter() {
+    var song25 = document.querySelector("#song25");
+    song25.play();
+}
+function song25leave () {
+    var song25 = document.querySelector("#song25");
+    song25.pause();
+}
+function song26enter() {
+    var song26 = document.querySelector("#song26");
+    song26.play();
+}
+function song26leave () {
+    var song26 = document.querySelector("#song26");
+    song26.pause();
+}
+function song27enter() {
+    var song27 = document.querySelector("#song27");
+    song27.play();
+}
+function song27leave () {
+    var song27 = document.querySelector("#song27");
+    song27.pause();
+}
+function song28enter() {
+    var song28 = document.querySelector("#song28");
+    song28.play();
+}
+function song28leave () {
+    var song28 = document.querySelector("#song28");
+    song28.pause();
+}
+function song29enter() {
+    var song29 = document.querySelector("#song29");
+    song29.play();
+}
+function song29leave () {
+    var song29 = document.querySelector("#song29");
+    song29.pause();
+}
+function song30enter() {
+    var song30 = document.querySelector("#song30");
+    song30.play();
+}
+function song30leave () {
+    var song30 = document.querySelector("#song30");
+    song30.pause();
+}
+function song31enter() {
+    var song31 = document.querySelector("#song31");
+    song31.play();
+}
+function song31leave () {
+    var song31 = document.querySelector("#song31");
+    song31.pause();
+}
+function song32enter() {
+    var song32 = document.querySelector("#song32");
+    song32.play();
+}
+function song32leave () {
+    var song32 = document.querySelector("#song32");
+    song32.pause();
+}
+function song33enter() {
+    var song33 = document.querySelector("#song33");
+    song33.play();
+}
+function song33leave () {
+    var song33 = document.querySelector("#song33");
+    song33.pause();
+}
+function song34enter() {
+    var song34 = document.querySelector("#song34");
+    song34.play();
+}
+function song34leave () {
+    var song34 = document.querySelector("#song34");
+    song34.pause();
+}
+function song35enter() {
+    var song35 = document.querySelector("#song35");
+    song35.play();
+}
+function song35leave () {
+    var song35 = document.querySelector("#song35");
+    song35.pause();
+}
+function song36enter() {
+    var song36 = document.querySelector("#song36");
+    song36.play();
+}
+function song36leave () {
+    var song36 = document.querySelector("#song36");
+    song36.pause();
+}
+function song37enter() {
+    var song37 = document.querySelector("#song37");
+    song37.play();
+}
+function song37leave () {
+    var song37 = document.querySelector("#song37");
+    song37.pause();
+}
+function song38enter() {
+    var song38 = document.querySelector("#song38");
+    song38.play();
+}
+function song38leave () {
+    var song38 = document.querySelector("#song38");
+    song38.pause();
+}
+function song39enter() {
+    var song39 = document.querySelector("#song39");
+    song39.play();
+}
+function song39leave () {
+    var song39 = document.querySelector("#song39");
+    song39.pause();
+}
+function song40enter() {
+    var song40 = document.querySelector("#song40");
+    song40.play();
+}
+function song40leave () {
+    var song40 = document.querySelector("#song40");
+    song40.pause();
+}
+function song41enter() {
+    var song41 = document.querySelector("#song41");
+    song41.play();
+}
+function song41leave () {
+    var song41 = document.querySelector("#song41");
+    song41.pause();
+}
+function song42enter() {
+    var song42 = document.querySelector("#song42");
+    song42.play();
+}
+function song42leave () {
+    var song42 = document.querySelector("#song42");
+    song42.pause();
+}
+function song43enter() {
+    var song43 = document.querySelector("#song43");
+    song43.play();
+}
+function song43leave () {
+    var song43 = document.querySelector("#song43");
+    song43.pause();
+}
+function song44enter() {
+    var song44 = document.querySelector("#song44");
+    song44.play();
+}
+function song44leave () {
+    var song44 = document.querySelector("#song44");
+    song44.pause();
+}
+function song45enter() {
+    var song45 = document.querySelector("#song45");
+    song45.play();
+}
+function song45leave () {
+    var song45 = document.querySelector("#song45");
+    song45.pause();
+}
+function song46enter() {
+    var song46 = document.querySelector("#song46");
+    song46.play();
+}
+function song46leave () {
+    var song46 = document.querySelector("#song46");
+    song46.pause();
+}
+function song47enter() {
+    var song47 = document.querySelector("#song47");
+    song47.play();
+}
+function song47leave () {
+    var song47 = document.querySelector("#song47");
+    song47.pause();
+}
+function song48enter() {
+    var song48 = document.querySelector("#song48");
+    song48.play();
+}
+function song48leave () {
+    var song48 = document.querySelector("#song48");
+    song48.pause();
+}
+function song49enter() {
+    var song49 = document.querySelector("#song49");
+    song49.play();
+}
+function song49leave () {
+    var song49 = document.querySelector("#song49");
+    song49.pause();
+}
+function song50enter() {
+    var song50 = document.querySelector("#song50");
+    song50.play();
+}
+function song50leave () {
+    var song50 = document.querySelector("#song50");
+    song50.pause();
+}
+function song51enter() {
+    var song51 = document.querySelector("#song51");
+    song51.play();
+}
+function song51leave () {
+    var song51 = document.querySelector("#song51");
+    song51.pause();
+}
+function song52enter() {
+    var song52 = document.querySelector("#song52");
+    song52.play();
+}
+function song52leave () {
+    var song52 = document.querySelector("#song52");
+    song52.pause();
+}
+function song53enter() {
+    var song53 = document.querySelector("#song53");
+    song53.play();
+}
+function song53leave () {
+    var song53 = document.querySelector("#song53");
+    song53.pause();
+}
+function song54enter() {
+    var song54 = document.querySelector("#song54");
+    song54.play();
+}
+function song54leave () {
+    var song54 = document.querySelector("#song54");
+    song54.pause();
+}
+function song55enter() {
+    var song55 = document.querySelector("#song55");
+    song55.play();
+}
+function song55leave () {
+    var song55 = document.querySelector("#song55");
+    song55.pause();
+}
+function song56enter() {
+    var song56 = document.querySelector("#song56");
+    song56.play();
+}
+function song56leave () {
+    var song56 = document.querySelector("#song56");
+    song56.pause();
+}
+function song57enter() {
+    var song57 = document.querySelector("#song57");
+    song57.play();
+}
+function song57leave () {
+    var song57 = document.querySelector("#song57");
+    song57.pause();
+}
+function song58enter() {
+    var song58 = document.querySelector("#song58");
+    song58.play();
+}
+function song58leave () {
+    var song58 = document.querySelector("#song58");
+    song58.pause();
+}
+function song59enter() {
+    var song59 = document.querySelector("#song59");
+    song59.play();
+}
+function song59leave () {
+    var song59 = document.querySelector("#song59");
+    song59.pause();
+}
+function song60enter() {
+    var song60 = document.querySelector("#song60");
+    song60.play();
+}
+function song60leave () {
+    var song60 = document.querySelector("#song60");
+    song60.pause();
+}
+function song61enter() {
+    var song61 = document.querySelector("#song61");
+    song61.play();
+}
+function song61leave () {
+    var song61 = document.querySelector("#song61");
+    song61.pause();
+}
+function song62enter() {
+    var song62 = document.querySelector("#song62");
+    song62.play();
+}
+function song62leave () {
+    var song62 = document.querySelector("#song62");
+    song62.pause();
+}
+function song63enter() {
+    var song63 = document.querySelector("#song63");
+    song63.play();
+}
+function song63leave () {
+    var song63 = document.querySelector("#song63");
+    song63.pause();
+}
+function song64enter() {
+    var song64 = document.querySelector("#song64");
+    song64.play();
+}
+function song64leave () {
+    var song64 = document.querySelector("#song64");
+    song64.pause();
+}
